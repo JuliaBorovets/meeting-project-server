a80d483bb1242a44d4babd2b702e8ee23f5c1a61
Julia Borovets <juliaborovets2001@gmail.com>
2020-09-13 13:48:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Meeting organizer project
+
+[![Build Status](https://travis-ci.com/JuliaBorovets/meeting-project.svg?branch=master)](https://travis-ci.com/JuliaBorovets/meeting-project) 
+[![codecov](https://codecov.io/gh/JuliaBorovets/meeting-project/branch/master/graph/badge.svg)](https://codecov.io/gh/JuliaBorovets/meeting-project)
+
---
+++ src/main/java/com/training/meeting/MeetingApplication.java
@@ -0,0 +1,13 @@
+package com.training.meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingApplication.class, args);
+    }
+
+}
